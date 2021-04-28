9796c5a25a2247b206762e8fd6d2e8110abe1892
annietrinh <annietrinh@localhost>
2021-04-28 09:25:14 -0700

    Ghost class

---
+++ MinecraftPacman/src/Ghost.java
@@ -0,0 +1,49 @@
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Rectangle;
+import java.awt.Toolkit;
+import java.awt.geom.AffineTransform;
+import java.net.URL;
+
+public class Ghost {
+	private int x, y;
+	private int vx, vy;
+	private int width, height;
+	
+	private Image img;
+	
+	public Ghost() {
+		
+		/*
+		x =  ;
+		y =  ;
+		vx = ;
+		vy = ;
+		width = ;
+		height = ;
+		img = getImage("???");	
+		init(x, y);
+		*/
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public void setVx(int vx) {
+		this.vx = vx;
+	}
+	
+	public int getVx() {
+		return this.vx;
+	}
+	public Rectangle getRect() {
+		Rectangle temp = new Rectangle(x,y,width,height);
+		return temp;
+	}
+}
