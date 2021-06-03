507a8dd541535f29c36f00267bd75f45fb6f25a0
annietrinh <annietrinh@192.168.1.8>
2021-06-03 09:24:11 -0700

    update class

--- MinecraftPacman/src/pacman/Fruit.java
+++ MinecraftPacman/src/pacman/Fruit.java
@@ -67,7 +67,7 @@ public class Fruit extends Coin {
 		}
 		return tempImage;
 	}
-	
+	//collision
 	public Rectangle getRect() {
 		Rectangle temp = new Rectangle(x ,y,width-10,height-10);
 		return temp;
--- MinecraftPacman/src/pacman/RegCoin.java
+++ MinecraftPacman/src/pacman/RegCoin.java
@@ -80,6 +80,8 @@ public class RegCoin extends Coin {
 	}
 
 	// setters and getters
+	
+
 
 
 	public int getX() {
